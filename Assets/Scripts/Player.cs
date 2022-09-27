2640d7142a5b3697b63ce50f367a0441d25182e6
mrmaxmax1423 <maximilianods@ymail.com>
2022-09-26 23:09:22 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+using Unity.Netcode;
+using UnityEngine;
+
+
+public class Player : NetworkBehaviour {
+
+
+    public NetworkVariable<Vector3> PositionChange = new NetworkVariable<Vector3>();
+    public NetworkVariable<Vector3> RotationChange = new NetworkVariable<Vector3>();
+    public NetworkVariable<Color> PlayerColor = new NetworkVariable<Color>(Color.red);
+
+    private GameManager _gameMgr;
+    private Camera _camera;
+    public float movementSpeed = .5f;
+    public float rotationSpeed = 1f;
+
+    private void Start()
+    {
+        ApplyPlayerColor();
+        PlayerColor.OnValueChanged += OnPlayerColorChanged;
+    }
+
+
+    public override void OnNetworkSpawn() {
+        _camera = transform.Find("Camera").GetComponent<Camera>();
+        if (IsOwner) {
+            _gameMgr = GameObject.Find("GameManager").GetComponent<GameManager>();
+            _gameMgr.RequestNewPlayerColorServerRpc();
+        }
+        _camera.enabled = IsOwner;
+    }
+
+
+    [ServerRpc]
+    void RequestPositionForMovementServerRpc(Vector3 posChange, Vector3 rotChange) {
+        if (!IsServer && !IsHost) return;
+
+        PositionChange.Value = posChange;
+        PositionChange.Value = rotChange;
+    }
+
+
+    public void OnPlayerColorChanged(Color previous, Color current) {
+        ApplyPlayerColor();
+    }
+
+    public void ApplyPlayerColor() {
+        GetComponent<MeshRenderer>().material.color = PlayerColor.Value;
+    }
+
+
+    private Vector3[] CalcMovement() {
+        bool isShiftKeyDown = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        float x_move = 0.0f;
+        float z_move = Input.GetAxis("Vertical");
+        float y_rot = 0.0f;
+
+        if (isShiftKeyDown)
+        {
+            x_move = Input.GetAxis("Horizontal");
+        }
+        else
+        {
+            y_rot = Input.GetAxis("Horizontal");
+        }
+
+        Vector3 moveVect = new Vector3(z_move, 0, x_move);
+        moveVect *= movementSpeed;
+
+        Vector3 rotVect = new Vector3(0, y_rot, 0);
+        rotVect *= rotationSpeed;
+
+        return new[] { moveVect, rotVect };
+    }
+
+
+    void Update() {
+        if (IsOwner) {
+            Vector3[] results = CalcMovement();
+            RequestPositionForMovementServerRpc(results[0], results[1]);
+        }
+
+        if (!IsOwner || IsHost){        
+            transform.Translate(PositionChange.Value);
+            transform.Rotate(RotationChange.Value);
+        }
+    }
+}
